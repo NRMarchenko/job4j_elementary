79f4c46ca040303abb7e76d9f1a7d80abb712945
Kirill Marchenko <mar4elos_91@mail.ru>
2024-10-11 17:51:41 +0300

    Фиксация изменений на github [#505270]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -15,7 +15,7 @@ class ConverterTest {
 
     @Test
     void whenConvert180RblThen3Dollar() {
-        float input = 180;
+        float input = 225;
         float expected = 3;
         float output = Converter.rubleToDollar(input);
         float value = 0.0001f;
@@ -24,7 +24,7 @@ class ConverterTest {
 
     @Test
     void whenConvert200RblThen3dot3333Euro() {
-        float input = 200;
+        float input = 250;
         float expected = 3.3333f;
         float output = Converter.rubleToDollar(input);
         float value = 0.0001f;
