9c211195db73cd33f2235b6f98efac319a12a85b
Kirill Marchenko <mar4elos_91@mail.ru>
2024-10-11 17:48:01 +0300

    Фиксация изменений на github [#505270]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -8,16 +8,19 @@ public class MathCalculator {
         return sum(first, second)
                 + multiply(first, second);
     }
+
     public static double differenceAndDivision(double first, double second) {
         return difference(first, second)
                 + division(first, second);
     }
+
     public static double allFunctions(double first, double second) {
         return sum(first, second)
                 + multiply(first, second)
                 + difference(first, second)
                 + division(first, second);
     }
+
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета равен: " + differenceAndDivision(20, 10));
