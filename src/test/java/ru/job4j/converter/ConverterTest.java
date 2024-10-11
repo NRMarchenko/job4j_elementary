93c8a09da6759a9801d890979c62fe7a440a52bb
Kirill Marchenko <mar4elos_91@mail.ru>
2024-10-11 17:52:58 +0300

    Фиксация изменений на github [#505270]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 class ConverterTest {
     @Test
     void whenConvert140RblThen2Euro() {
-        float input = 140;
+        float input = 180;
         float expected = 2;
         float output = Converter.rubleToEuro(input);
         float value = 0.0001f;
