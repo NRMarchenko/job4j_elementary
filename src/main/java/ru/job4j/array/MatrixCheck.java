14e06436440cfd8510013570f700587602635412
Kirill Marchenko <mar4elos_91@mail.ru>
2024-10-28 22:15:06 +0300

    Фиксация изменений на github [#505270]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        /* Поменял проверку - эта строка чтобы сделать коммит новый*/
         for (int i = 0; i < board[row].length; i++) {
             if (board[row][i] != 'X') {
                 result = false;
@@ -15,9 +14,8 @@ public class MatrixCheck {
 
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        /* Поменял проверку - эта строка чтобы сделать коммит новый*/
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][column] != 'X') {
+        for (char[] chars : board) {
+            if (chars[column] != 'X') {
                 result = false;
                 break;
             }
