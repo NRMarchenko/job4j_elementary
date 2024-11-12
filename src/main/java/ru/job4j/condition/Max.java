9ccc197ae627ae0106bcccb05cb1ba8361fb4a27
Kirill Marchenko <mar4elos_91@mail.ru>
2024-11-12 22:10:39 +0300

    Фиксация изменений на github [#505270]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,23 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    private int a;
-    private int b;
-    private int c;
-
     public static int max(int a, int b) {
-        int max = (a > b) ? a : b;
-        return max;
+        return (a > b) ? a : b;
     }
 
     public static int max(int a, int b, int c) {
-        int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-        return max;
+        return max(max(a, b), c);
     }
 
     public static int max(int a, int b, int c, int d) {
-        int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-        max = (max > d) ? max : d;
-        return max;
+        return max(max(a, b), max(c, d));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -3,8 +3,6 @@ package ru.job4j.condition;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.withPrecision;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MaxTest {
 
